f01ca0c69cd4f7444534fb1b8c480893f4d0f15a
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-06-14 10:04:45 +0300

    "Feat: adds the app forms"

---
+++ DevAwards/forms.py
@@ -0,0 +1,20 @@
+from django.forms import ModelForm
+from . models import *
+class AddProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = ['title','description','image','url']
+        widgets= {
+            'url':forms.Textarea()
+        }
+class UpdateProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio','profile_photo']
+class RatingForm(forms.ModelForm):
+  class Meta:
+    model = Ratings
+    fields = ['design', 'userbility', 'content']
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        
